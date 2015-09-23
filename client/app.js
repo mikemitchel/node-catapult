589c91b48754e4b99249ea4e8afa13a0f395464b
Lecture Room <lectureroom@Lectures-Mac-mini.local>
2015-09-23 12:14:42 -0500

    Lecture notes

--- client/app.js
+++ client/app.js
@@ -4,13 +4,63 @@ var MyComponent = require('./components/MyComponent')
 
 window.App = {}
 
-App.controller = function () {}
+App.controller = function () {
+  var ctrl = this
+
+  // Example of controller holding view-state
+  ctrl.userInput = 'Hello'
+}
 
 App.view = function (ctrl) {
+  console.log("Redrawing")
+
   return [
     m('h1', 'Node Catapult'),
-    m.component(MyComponent, { title: 'Welcome to my app!' })
+
+    m('p', ctrl.userInput),
+
+    // Example of two-way data binding
+    m('input', {
+      value: ctrl.userInput,
+      oninput: function(e) {
+        ctrl.userInput = e.currentTarget.value
+      }
+    }),
+
+    // Example of integrating with a 3rd party lib (select2)
+    m('select', {
+      config: function (elem, hasInitialized) {
+        if (hasInitialized) return;
+        // $(elem).select2();
+      }
+    }, [
+      m('option', 'One'),
+      m('option', 'Two')
+    ]),
+
+    // Example of a helper view
+    personView({ name: "Alice" }),
   ]
 }
 
+function personView (person) {
+  return m('h5', person.name)
+}
+
+// This puts our component on the page
 m.mount(document.getElementById('app'), App)
+
+//
+// This is what m.mount basically does with your component
+//
+// var componentCtrl = new App.controller()
+// var virtualDom = App.view(componentCtrl)
+
+
+//
+// Example of using routes (m.route replaces m.mount)
+//
+// m.route(document.getElementById('app'), '/', {
+//   '/': App,
+//   '/users': UserList
+// })
--- client/components/MyComponent.js
+++ client/components/MyComponent.js
@@ -1,9 +1,17 @@
 var m = require('mithril')
+window.m = m
 
 exports.controller = function (options) {}
 
 exports.view = function (ctrl, options) {
-  return m('.my-component', [
-    m('h2', options.title)
+  return m('div.my-component', [
+    m('h2', options.title),
+    m('p', "This is some text")
   ])
 }
+
+
+// Downsides of Mithril
+//   - Smaller community
+//   - Not as popular as other things
+//   - More DIY
